a866cf0f437a6cb51d967724f7b9df5a7c353776
Doarn <Ian.Doarn@zimmer.com>
2016-11-01 10:36:29 -0500

    Biomet Queries Update

---
+++ Bins/biomet_kit_serial_qty.sql
@@ -0,0 +1,34 @@
+﻿SELECT 
+  p2.product_number as kit_prod_number,
+  p2.edi_number as kit_edi,
+  p2.description as kit_description,
+  ps.serial_number,
+  b.zone || '-' || b.position || '-' || b.shelf as kit_bin,
+  p.product_number as Component_product_number,
+  p.edi_number as component_edi,
+  p.description as component_description,
+  sum (s.quantity_available) as quantity_available
+FROM
+  sms.stock s 
+  LEFT JOIN sms.product p on s.product_id = p.id
+  LEFT JOIN sms.stock s2 On s.container_id = s2.id and s.container_type = 2
+  LEFT JOIN sms.product p2 on s2.product_id = p2.id
+  left join sms.product_serial ps on s2.serial_id = ps.id
+  LEFT JOIN sms.bin b on s2.container_id = b.id and s2.container_type = 1
+where 
+  s.location_type = 1
+  and s.location_id = 370
+  and s.stock_type = 2
+  and s.container_type =2
+  and p.is_biomet = 1
+  and p2.is_biomet = 1
+  and p2.product_number is not null
+GROUP BY
+  p2.product_number ,
+  p2.edi_number,
+  p2.description,
+  ps.serial_number,
+  b.zone || '-' || b.position || '-' || b.shelf ,
+  p.product_number ,
+  p.edi_number ,
+  p.description
---
+++ Reports/valid-invalid-formulas_biomet.sql
@@ -0,0 +1,29 @@
+﻿with s1 as (
+SELECT 
+  p.product_number, p.edi_number, p.description,
+  case when stock_type = 3 then sum(s.quantity_on_hand) end as Valid,
+  case when stock_type = 4 then sum(s.quantity_on_hand) end as Invalid
+FROM 
+  sms.stock s
+    LEFT JOIN sms.product p ON s.product_id = p.id
+WHERE
+  s.inventory_type = 3
+  and stock_type in (3,4)
+  and p.is_biomet = 1
+group by
+  p.product_number,p.edi_number, p.description,
+  s.stock_type),
+
+s2 as (
+Select
+  s1.product_number, s1.edi_number, s1.description, 
+  coalesce(sum(s1.valid),0) as valid,
+  coalesce(sum (s1.invalid),0) as invalid
+from
+ s1
+Group by 
+  product_number, edi_number, description)
+SELECT
+  s2.* 
+from
+  s2
